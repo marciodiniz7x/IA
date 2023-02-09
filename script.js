71215f4bcd114e2d8a8c931a2b17c80067caa69e
Márcio Diniz <marcio.sccp10@gmail.com>
2023-02-09 12:21:41 -0300

    Atts

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ async function calcular() {
     const net = brain.NeuralNetwork({ hiddenLayers: [1]});
     // Forneceu dados para treinamento da rede neural
     const training = [
-        { input: [0, 0, 1], output: [0]}
+        { input: [0, 0, 1], output: [0] },
+        { input: [0, 1, 1], output: [0] },
+        { input: [0, 1, 0], output: [0] },
+        { input: [1, 0, 1], output: [1] },
+        { input: [1, 1, 0], output: [1] },
     ];
+    // Rede neural treina com base nos dados
+    
 
 }

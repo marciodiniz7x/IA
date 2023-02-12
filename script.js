14a32b8b7af89040633434871dbcb6c34feffa5c
Márcio Diniz <marcio.sccp10@gmail.com>
2023-02-12 11:03:32 -0300

    Atts

--- script.js
+++ script.js
@@ -1,39 +1,81 @@
 const resultado = document.querySelector('.resultado');
 const porque = document.querySelector('.porque');
+const progress = document.querySelector('.progress');
+const container2 = document.querySelector('.container2');
+const justificativa = document.querySelector('.justificativa');
 
-async function calcular() {
+let time;
+let finalperc;
+
+function calcular() {
 
     // Instanciou a rede neural
-    const net = new brain.NeuralNetwork({ hiddenLayers: [4], activation: 'sigmoid'});
+    const net = new brain.NeuralNetwork({ hiddenLayers: [3], activation: 'sigmoid'});
     // Forneceu dados para treinamento da rede neural
     const dados = [
-        // O output mostra o time vencedor após um retrospecto de quem venceu os últimos 6 jogos inseridos no input, ignorando os empates, sendo o 1 o time mandante no último jogo.
-        { input: [1, 1, 1, 1, 0, 1], output: [0] },
-        { input: [0, 0, 0, 1, 1, 1], output: [1] },
-        { input: [0, 0, 1, 0, 0, 0], output: [0] },
-        { input: [1, 0, 0, 1, 1, 1], output: [0] },
-        { input: [0, 1, 1, 1, 0, 0], output: [1] },
+
+        
+    // Regra I -> número 1: Adiciona peso ao PSG e número 0: Adiciona peso ao Bayern
+
+    // Regra II -> Vitória e empate fora, adiciona peso ao vencedor e derrota adiciona peso ao adversário
+
+
+    //  { input: [A, B, C, D], output: [quem pontuar mais] }
+    //  A: Jogo do PSG
+    //  B: Jogo do Bayern
+    //  C: Quem fez mais gols em seus jogos
+    //  D: Quem venceu em confronto direto
+        
+        
+        { input: [0, 0, 0], output: [0] },
+        { input: [1, 0, 0], output: [0] },
+        { input: [1, 1, 1], output: [1] },
+        { input: [1, 1, 1], output: [1] },
+        
 
     ];
     // Rede neural treina com base nos dados
     net.train(dados);
+
     // Rede neural Lê os dados a partir dos quais ela terá que adivinhar o resultado
-    const output = net.run([0, 1, 0, 0, 0, 1]);
+    const output = net.run([0, 1, 0]);
     const arredond = Math.round(output);
-    // Substitui o valor 0 e 1 pelos times.
+
+    // Calcula a porcentagem de acerto
+    let percent = output*100;
+    let coef = 50 - percent;
+    if (percent > 50) {
+        finalperc = percent
+    } else if (percent < 50) {
+        finalperc = 50 + coef;
+    }
     
+    // Substitui o valor 0 e 1 pelos times.
     if ( arredond === 1 ) {
-        var time = "PSG";
+        time = "PSG";
     } else if ( arredond === 0 ) {
-        var time = "Bayern";
+        time = "Bayern";
     }
 
     // Mostrar resultado da previsão no html
     resultado.classList.add('surgir');
+    resultado.classList.remove('piscar')
+    porque.classList.add('surgir');
+    porque.classList.add('sumir');
+    progress.classList.remove('movimentar');
+    progress.style.marginLeft = `${percent}px`;
     setTimeout(() => {
         resultado.classList.remove('surgir');
-        porque
+        porque.classList.remove('sumir');
     }, 1200);
-    resultado.innerHTML = `Pelo modelo dado, creio que quem vence é o: <span style="color:#d8fa7c;"><b>${time}</b></span>.`;
+    resultado.innerHTML = `Pelo modelo dado, avalio que quem vence é o: <span style="color:#d8fa7c;"><b>${time}</b></span>.<br>`;
+
+}
+
+function justificar() {
+
+    container2.classList.remove('sumir');
+
+    justificativa.innerHTML = `Com base no histórico de partidas dos times em seus campeonatos, adicionando pesos diferentes para jogos em casa e fora, bem como os confrontos diretos entre os dois times, avalio o <span style="color:#d8fa7c;"><b>${time}</b></span> tem <span style="color:#d8fa7c;"><b>${Math.round(finalperc)}%</b></span> de chance de vencer jogando em casa.`;
 
 }
